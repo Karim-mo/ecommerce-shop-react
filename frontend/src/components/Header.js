52de137004fbd7bb207cdb4604b808cf4dfa9494
Suicidal <karim.mohamed420@gmail.com>
2021-04-20 04:36:24 +0200

    Created header/footer components

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,26 @@
+import { Container, Row, Col, Nav, Navbar } from 'react-bootstrap';
+
+const Header = () => {
+	return (
+		<div>
+			<Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+				<Navbar.Brand href='/'>Quos Online Shopping</Navbar.Brand>
+				<Container>
+					<Navbar.Toggle aria-controls='basic-navbar-nav' />
+					<Navbar.Collapse id='basic-navbar-nav'>
+						<Nav className='ml-auto'>
+							<Nav.Link href='/cart'>
+								<i className='fas fa-shopping-cart'></i> Cart
+							</Nav.Link>
+							<Nav.Link href='/login'>
+								<i className='fas fa-user'></i> Login
+							</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</div>
+	);
+};
+
+export default Header;
